aad02a39b69bee9b3124fb870259db9eb8cd1960
CryTek-Team <ahpgs83@gmail.com>
2016-04-25 13:59:17 +0430

    Create add_plug.lua

---
+++ plugins/add_plug.lua
@@ -0,0 +1,24 @@
+#addplug local function run(msg, matches)
+  if is_sudo(msg) then
+  local text = matches[1]
+  local b = 1
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  local name = matches[2]
+  local file = io.open("./plugins/"..name..".lua", "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  return "درپوشه پلاگین ها اضافه شد و در لیست کانفیگ نیز اضافه گشت برای بروز اوری بنویسید  /reload" 
+end
+end 
+return {
+  description = "a Usefull plugin for sudo !",  
+  usage = "A plugins to add Another plugins to the server",
+  patterns = {
+    "^[!/]addplug +(.+) (.*)$"
+  },
+  run = run
+} addplug
