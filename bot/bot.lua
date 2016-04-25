995fca0cf4c22cb8f639c0e70261e26b04d2d260
CryTek-Team <ahpgs83@gmail.com>
2016-04-25 09:19:46 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -274,7 +274,7 @@ function create_config( )
       "nerkharz",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {176619893},
     admin_users = {},
     disabled_channels = {}
   }
