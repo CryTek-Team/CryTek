440297ffcdaba17fc316cc5360b4ca6f4ecb0c7d
CryTek-Team <ahpgs83@gmail.com>
2016-04-27 15:57:30 +0430

    Update anti_tag.lua

--- plugins/anti_tag.lua
+++ plugins/anti_tag.lua
@@ -8,10 +8,10 @@ local function run(msg)
 if not is_momod(msg) then
 
 
-chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+chat_del_user('channel#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
     local msgtag = 'You cant tag anything here '
    local receiver = msg.to.id
-    send_large_msg('channel#id'..receiver, msgads.."\n", ok_cb, false)
+    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
 
       end
    end
